11a0005e6143dbabe018724051e5bfcf37d3ad61
aseelflihan <aseelflihan@gmail.com>
2023-03-21 13:15:42 +0300

    complate assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div> const links = [
+    {text: "Home", path: "/"},
+    {text: "Movies", path: "/movies"},
+    {text: "Directors", path: "/directors"},
+    {text: "Actors", path: "/actors"},
+  ]
+  return <ul className="navbar">
+    {links.map(link =>
+        <li key={link.text}>
+          <NavLink exact to={link.path}>
+            {link.text}
+          </NavLink>
+        </li>)}
+  </ul>;
+  </div>;
 }
 
 export default NavBar;
